ae5dd0ca2c36eb201e79a92f818f364de24061dd
Vlad <gvladislav96@gmail.com>
2024-09-16 09:43:44 +0500

    updateView

---
+++ H.W2.1/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  H.W2.1
+//
+//  Created by  Влад on 16.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let name = "Vlad"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("Меня зовут \(name), я создал свое первое приложение")
+       
+    }
+
+
+}
+
